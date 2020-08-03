1533a9927c5ac435cb0b0355c7f6fdc03ecaa02a
peer35 <p.j.m.vos@vu.nl>
2020-08-03 15:44:44 +0200

    guid list

--- rename_pdf.py
+++ rename_pdf.py
@@ -2,17 +2,17 @@ import shutil
 from bs4 import BeautifulSoup
 import lxml
 
-pdf_path='c:/Users/peter/Documents/lac2014/pdf'
+pdf_path = 'c:/Users/peter/Documents/lac2014/pdf'
 
 with open("xml/crossref.xml", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, "xml")
-    doi_data=soup.find_all("doi_data")
+    doi_data = soup.find_all("doi_data")
     for d in doi_data:
         id = d.doi.string[8:len(d.doi.string)]
         print(id)
-        f=d.resource.string[-2:]
-        filename=f'{f}-{f}-1-PB.pdf'
+        f = d.resource.string[-2:]
+        filename = f'{f}-{f}-1-PB.pdf'
         print(filename)
-        old_name=f'{pdf_path}/{filename}'
-        new_name=f'{pdf_path}/renamed/{id}.pdf'
+        old_name = f'{pdf_path}/{filename}'
+        new_name = f'{pdf_path}/renamed/{id}.pdf'
         shutil.copy(old_name, new_name)
---
+++ xml/osf_guid_list.py
@@ -0,0 +1,49 @@
+import json
+
+import requests
+from config import LAC_NODE_ID, OSF_TOKEN
+
+def getOSFStorageFiles(node_id=LAC_NODE_ID, url = False):
+    headers={ 'Authorization': f'Bearer {OSF_TOKEN}' }
+    if (url):
+        res = requests.get(url, headers=headers)
+    else:
+        res = requests.get(f'https://api.osf.io/v2/nodes/{node_id}/files/osfstorage?format=json', headers=headers)
+    print(res.url)
+    return res.json()
+
+def getFileById(id):
+    #5f28048b9c9094019048927f
+    headers={ 'Authorization': f'Bearer {OSF_TOKEN}' }
+    res = requests.get(f' https://osf.io/{LAC_NODE_ID}/files/osfstorage/{id}/', headers=headers)
+    print(res.url)
+
+def _store(data, filename):
+    '''
+    Store the data dict as a json file
+
+    :param data: dict
+    :param filename: string
+    :return:
+    '''
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=1)
+
+osf_api_data=getOSFStorageFiles()
+next = True
+data=[]
+while next:
+    for d in osf_api_data['data']:
+        if d['attributes']['kind']=='file':
+            print(d)
+            print(d['attributes']['name'])
+            print(d['attributes']['guid'])
+            if d['attributes']['guid']==None: # touch the file to generate a guid
+                getFileById(d['id'])
+            data.append({'name': d['attributes']['name'], 'guid': d['attributes']['guid']})
+    if osf_api_data['links']['next']!=None:
+        osf_api_data=getOSFStorageFiles(url=osf_api_data['links']['next'])
+    else:
+        next=False
+
+_store(data, 'name_guid.json')
